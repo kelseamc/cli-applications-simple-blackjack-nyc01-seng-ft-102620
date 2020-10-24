a91acc1feca63b431da9a9640a5f5d3b1de62256
Kelsea McAllister <kelsea.mcall@gmail.com>
2020-10-24 12:52:49 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -18,8 +18,10 @@ def get_user_input
   gets.chomp
 end
 
-def end_game
-  # code #end_game here
+def end_game(total)
+  if total > 21
+    puts "Sorry, you hit #{card_total}. Thanks for playing!"
+  end 
 end
 
 def initial_round
